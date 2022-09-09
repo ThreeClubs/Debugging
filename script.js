f2dcbac0a7c6a671401472337480dc333491d388
Andrew Grenon <andrew.grenon@gmail.com>
2022-09-09 12:08:30 +1200

    debugging

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log("test");
+
+//Steps to take when de-bugging
+
+// step 1: check the console for an error message and read the message carefully
+
+// step 2: use the browser debugger to step through code and find out where it's going wrong
+
+// let test = 1;
+// console.log(test);
+
+// function theTest() {
+//     console.log(quarks);
+// }
+
+// theTest();
+
+let fruit = "bAnanA";
+
+if (fruit === "Banana") {
+    console.log("Banana");
+} else {
+    console.log("Not Banana");
+};
+
+//step 3: google stack overflow or gitHub
+
+// step 4: Maybe ask a colleague
